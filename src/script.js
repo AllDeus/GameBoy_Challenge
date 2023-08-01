8e7db0308fa32e8a0145c14d9396c4fb015dab1d
AllDeus <russiffilli@gmail.com>
2023-08-01 12:23:22 -0400

    commit to master

--- src/script.js
+++ src/script.js
@@ -65,34 +65,23 @@ gltfLoader.load(
     }
 );
 
-// Animation loop
-const animate = () => {
-    requestAnimationFrame(animate);
-
-    // Rotate the cube just for illustration purposes
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-};
 
 
 
+/** Weather */
 
 
-
-/**
- * Fog
- */
+// Fog
 const fog = new THREE.Fog('#262837', 1, 15)
 scene.fog = fog
+
 
 
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+const ambientLight = new THREE.AmbientLight(0x555555)
 
 scene.add(ambientLight)
 
@@ -108,7 +97,6 @@ gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
-
 
 
 
@@ -150,6 +138,12 @@ const controls = new OrbitControls(camera, canvas)
 controls.target.set(0, 1, 0)
 controls.enableDamping = true
 
+// Limit vertical rotation angle (avoid looking below the 3D model)
+const minPolarAngle = Math.PI / 4; // Minimum vertical angle in radians (45 degrees)
+const maxPolarAngle = Math.PI / 2.5; // Maximum vertical angle in radians (70 degrees)
+controls.minPolarAngle = minPolarAngle;
+controls.maxPolarAngle = maxPolarAngle;
+
 /**
  * Audio
  */
@@ -183,10 +177,67 @@ const renderer = new THREE.WebGLRenderer({
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setClearColor('#000000')
+renderer.setClearColor('#26333e')
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+
+
+// Firefly particle system
+const fireflyCount = 30;
+const fireflyGeometry = new THREE.BufferGeometry();
+const fireflyPositions = new Float32Array(fireflyCount * 3);
+const fireflySpeeds = new Float32Array(fireflyCount);
+const fireflyColors = new Array(fireflyCount);
+
+for (let i = 0; i < fireflyCount; i++) {
+    fireflyPositions[i * 3 + 0] = (Math.random() - 0.5) * 10; // X position
+    fireflyPositions[i * 3 + 1] = (Math.random() - 0.5) * 5; // Y position
+    fireflyPositions[i * 3 + 2] = (Math.random() - 0.5) * 5; // Z position
+    fireflySpeeds[i] = 0.01 + Math.random() * 0.04; // Speed of the firefly 
+    fireflyColors[i] = new THREE.Color(Math.random(), Math.random(), Math.random()); // Random color for each firefly
+}
+
+
+fireflyGeometry.setAttribute('position', new THREE.BufferAttribute(fireflyPositions, 3));
+fireflyGeometry.setAttribute('speed', new THREE.BufferAttribute(fireflySpeeds, 1));
+
+const fireflyMaterial = new THREE.ShaderMaterial({
+    vertexShader: `
+      attribute float speed;
+      varying vec3 vColor;
+  
+      void main() {
+        vColor = color;
+        vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+        gl_PointSize = 4.0;
+        gl_Position = projectionMatrix * modelViewPosition;
+      }
+    `,
+    fragmentShader: `
+      varying vec3 vColor;
+  
+      void main() {
+        gl_FragColor = vec4(vColor, 1.0);
+      }
+    `,
+    transparent: false,
+    vertexColors: true,
+});
+const fireflyParticleSystem = new THREE.Points(fireflyGeometry, fireflyMaterial);
+scene.add(fireflyParticleSystem);
+
+
+// FireFly Light effect
+const light = new THREE.PointLight(0xffffff, 1.0); // White light with full intensity
+scene.add(light);
+
+
+// function updateLightIntensity() {
+//     const time = Date.now() * 0.001; // Convert milliseconds to seconds
+//     const intensity = 0.5 + 0.5 * Math.sin(time * 2.0); // Adjust the frequency here
+//     light.intensity = intensity;
+// }
 
 /**
  * Animate
@@ -200,8 +251,39 @@ const tick = () => {
 
 
     if (mixer) {
-        mixer.update(deltaTime)
+        mixer.update(elapsedTime)
     }
+
+
+    // Move the fireflies around smoothly
+    const positions = fireflyGeometry.attributes.position.array;
+    const speeds = fireflyGeometry.attributes.speed.array;
+    for (let i = 0; i < fireflyCount; i++) {
+        const x = positions[i * 3 + 0];
+        const y = positions[i * 3 + 1];
+        const z = positions[i * 3 + 2];
+
+        // Calculate the new position using lerp
+        positions[i * 3 + 0] = THREE.MathUtils.lerp(x, x + speeds[i] * (Math.random() - 0.5), 0.1);
+        positions[i * 3 + 1] = THREE.MathUtils.lerp(y, y + speeds[i] * (Math.random() - 0.5), 0.1);
+        positions[i * 3 + 2] = THREE.MathUtils.lerp(z, z + speeds[i] * (Math.random() - 0.5), 0.1);
+
+        // Wrap around the scene if they go out of bounds
+        positions[i * 3 + 0] = (positions[i * 3 + 0] + 5) % 10 - 5;
+        positions[i * 3 + 1] = (positions[i * 3 + 1] + 5) % 10 - 5;
+        positions[i * 3 + 2] = (positions[i * 3 + 2] + 5) % 10 - 5;
+    }
+
+    fireflyGeometry.attributes.position.needsUpdate = true;
+
+
+    // Update the light positions to match the fireflies' positions
+    light.position.set(camera.position.x, camera.position.y, camera.position.z);
+
+    // // Update light intensity
+    // updateLightIntensity();
+
+
 
 
 
